ba8e6a12429dd1629f8b9e8367cba06e1a055e2c
Your Name <nikamtiuli289@gmail.com>
2025-12-23 01:10:05 -0800

    couple pages update

--- src/main/java/com/app/FifthController.java
+++ src/main/java/com/app/FifthController.java
@@ -1,39 +1,17 @@
 package com.app;
 
-import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class MainController {
+public class FifthController {
 
     @FXML
-    private Label statusLabel;
-
-    @FXML
-    private MFXButton actionButton;
-
-    @FXML
-    private void handleButtonClick(ActionEvent event) throws Exception {
-        // Load second page
-        Parent root = FXMLLoader.load(getClass().getResource("/second.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 800, 600);
-        stage.setScene(scene);
-        stage.show();
-
-
-
-        // Optional: remove the time text update if you don't want it
-        // statusLabel.setText("...");
-    }
-    @FXML
-    private void goToSecond(ActionEvent event) throws Exception {
+    private void goBackToSecond(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/second.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 800, 600);
@@ -41,9 +19,26 @@ public class MainController {
         stage.show();
     }
 
+    @FXML
+    private void goToMain(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
     private void goToThird(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/third.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goToFourth(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fourth.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
--- src/main/java/com/app/ThirdController.java
+++ src/main/java/com/app/ThirdController.java
@@ -8,11 +8,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class SecondController {
+public class ThirdController {
 
     @FXML
-    private void goBack(ActionEvent event) throws Exception {
-        // Load and switch back to main.fxml (Page 1)
+    private void goBackToSecond(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/second.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goToMain(ActionEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 800, 600);
@@ -21,8 +29,8 @@ public class SecondController {
     }
 
     @FXML
-    private void goToThird(ActionEvent event) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/third.fxml"));
+    private void goToFourth(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fourth.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
